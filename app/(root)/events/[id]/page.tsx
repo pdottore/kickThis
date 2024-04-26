c2443da0beef9287af105fed5d0d3cf376b8e1b6
pdottore <patrickdottore@gmail.com>
2024-04-26 18:48:13 -0400

    stripe

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CheckoutButton from "@/components/shared/CheckoutButton";
 import Collection from "@/components/shared/Collection";
 import { getEventById, getRelatedEventsByCategory } from "@/lib/actions/event.actions"
 import { formatDateTime } from "@/lib/utils";
@@ -48,6 +49,8 @@ SearchParamProps) => {
             </div>
 
             {/* CHECKOUT BUTTON */}
+            <CheckoutButton event={event} />
+
 
             <div className="flex flex-col gap-5">
               <div className="flex gap-2 md:gap-3">
